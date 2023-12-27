84ce97e2b82175f39a917e7502f291b16325792d
昵称8aflkz1l2q <17264256@gitlab.com>
2023-12-27 04:12:31 +0000

    Location.java

---
+++ src/Entity/Location.java
@@ -0,0 +1,51 @@
+package Entity;
+
+import Main.GamePanel;
+
+public class Location {
+    private int xPosition;
+    private int yPosition;
+
+    public Location(int xPosition, int yPosition) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    /**
+     * 判断向指定方向是否可以移动
+     * @param entity
+     * @param direction
+     * @return
+     */
+    public static boolean canMove(Entity entity, Direction direction){
+      return true;
+    }
+
+
+
+    /**
+     * 向指定方向移动一步(不作碰撞检测)
+     * @param entity
+     * @param direction
+     */
+    public static void moveOneStep(Entity entity, Direction direction) {
+            entity.loc.setXPosition(entity.loc.getXPosition() + direction.getDx());
+            entity.loc.setYPosition(entity.loc.getYPosition() + direction.getDy());
+    }
+}
